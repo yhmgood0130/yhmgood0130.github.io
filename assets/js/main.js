45483c5dbefa5a7c0bfa99e5d8bd5843ba36b72c
Hyunmo <yhmgood0130@gmail.com>
2017-09-28 01:29:58 -0600

    Portfolio is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
   	$("#bunny").delay(50).fadeOut("slow");
+    $('#myCarousel').carousel({
+        interval: false
+    });
     showProject();
     showCode();
 })
 
-function showProject() {
-  console.log($(this).attr('id'));
+function showProject(id) {
+  if(id === 'genie'){
+    window.open("https://football-genie.firebaseapp.com");
+  } else if(id === 'gamerz'){
+    window.open("https://gamerz-79621.firebaseapp.com/");
+  } else if(id === 'reads'){
+    window.open("https://galvanize-reads-ec524.firebaseapp.com/");
+  }
 }
 
-function showCode() {
-
+function showCode(id) {
+  if(id === 'genie'){
+    window.open("https://github.com/yhmgood0130/Football-Genie");
+  } else if(id === 'gamerz'){
+    window.open("https://github.com/yhmgood0130/full-stack-web-client");
+  } else if(id === 'reads'){
+    window.open("https://github.com/yhmgood0130/galvanize-reads-client-side");
+  }
 }
